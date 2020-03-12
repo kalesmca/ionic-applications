4c46f0004fd6c42f814b1c8c462e2263ba697860
Kaleeswaran <kaleeswaran@iinerds.com>
2020-03-12 12:20:46 +0530

    save invoice done webservice pending

---
+++ myApp/src/app/config/constant.ts
@@ -0,0 +1,11 @@
+export class Constants {
+  constructor() {}
+  entryColumns = [
+    { id: 1, name: "Sno", colSize: 1 },
+    { id: 2, name: "Name", colSize: 3 },
+    { id: 3, name: "Type", colSize: 2 },
+    { id: 4, name: "Price", colSize: 2 },
+    { id: 5, name: "Qty", colSize: 1 },
+    { id: 6, name: "Amt", colSize: 3 }
+  ];
+}
--- myApp/src/app/folder/folder.page.ts
+++ myApp/src/app/folder/folder.page.ts
@@ -1,21 +1,29 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { Invoice, Entry } from '../models/invoice-model';
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
+import { Invoice, Entry } from "../models/invoice-model";
+import { Constants } from "../config/constant";
 
 @Component({
-  selector: 'app-folder',
-  templateUrl: './folder.page.html',
-  styleUrls: ['./folder.page.scss'],
+  selector: "app-folder",
+  templateUrl: "./folder.page.html",
+  styleUrls: ["./folder.page.scss"]
 })
 export class FolderPage implements OnInit {
   public folder: string;
   invoice = new Invoice();
   entry = new Entry();
-  gender: string = "m"
-
-  constructor(private activatedRoute: ActivatedRoute) { }
+  constants = new Constants();
+  constructor(private activatedRoute: ActivatedRoute) {}
   ngOnInit() {
-    this.folder = this.activatedRoute.snapshot.paramMap.get('id');
+    this.folder = this.activatedRoute.snapshot.paramMap.get("id");
+    console.log("constants::", this.constants);
   }
-
+  addItem() {
+    console.log("item entry ::", this.entry);
+    this.invoice.entryList.push(this.entry);
+    this.entry = new Entry();
+  }
+  clear() {
+    this.entry = new Entry();
+  }
 }
